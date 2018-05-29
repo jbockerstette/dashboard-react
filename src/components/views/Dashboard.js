5275805beb100871f56593b50aa847da4cc8ee8f
jbockerstette <jim.bockerstette@gmail.com>
2018-05-29 14:18:56 -0700

    Fixed my margins and padding.

--- src/components/views/Dashboard.js
+++ src/components/views/Dashboard.js
@@ -4,21 +4,18 @@ import Bills from '../containers/Bills';
 import Profile from '../containers/Profile';
 import './Dashboard.css';
 
-const Dashboard = props => {
-  console.log(props);
-  return (
-    <React.Fragment>
-      <div className="Dashboard-accounts">
-        <Accounts {...props} />
-      </div>
-      <div className="Dashboard-bills">
-        <Bills {...props} />
-      </div>
-      <div className="Dashboard-profile">
-        <Profile {...props} />
-      </div>
-    </React.Fragment>
-  );
-};
+const Dashboard = props => (
+  <React.Fragment>
+    <div className="Dashboard-accounts">
+      <Accounts {...props} />
+    </div>
+    <div className="Dashboard-bills">
+      <Bills {...props} />
+    </div>
+    <div className="Dashboard-profile">
+      <Profile {...props} />
+    </div>
+  </React.Fragment>
+);
 
 export default Dashboard;
